b14a52fc60a9e05fc28f9422a2c17d2f0c16dcfd
Yingbo <yingbo_liu@brown.edu>
2017-04-09 14:16:21 +0800

    adding java implementation

--- top-k-largest.py
+++ top-k-largest.py
@@ -24,3 +24,33 @@ class Solution:
 a = [3,10,1000,-99,4,100]
 s = Solution()
 print(s.topK(a, 3))
+
+
+
+###JAVA implementation
+class Solution {
+
+     public int[] topk(int[] nums, int k) {
+         PriorityQueue<Integer> minheap = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
+             public int compare(Integer o1, Integer o2) {
+                 if(o1 < o2) {
+                     return 1;
+                 } else if(o1 > o2) {
+                     return -1;
+                 } else {
+                     return 0;
+                 }
+             }
+         });
+
+         for (int i : nums) {
+             minheap.add(i);
+         }
+
+         int[] result = new int[k];
+         for (int i = 0; i < result.length; i++) {
+             result[i] = minheap.poll();
+         }
+         return result;
+    }
+};
